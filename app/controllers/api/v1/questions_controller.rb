ee66acd689b2ba87c877600bdcb7a99a563cfc2f
Eric Luby <60050306+ericluby@users.noreply.github.com>
2020-10-29 21:54:54 -0400

    added question form container and question index container

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::QuestionsController < ApiController
   def index
+    binding.pry
+    render json: Question.all
+  end
+
+  def create
+    binding.pry
+    question = Question.new(status: "new", title: params["title"])
+    question.user = current_user
+    question.save
+
+    render json: question
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/questions', to: "homes#index"
-
-  resources :questions, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :questions, only: [:index, :create]
+    end
+  end
 end

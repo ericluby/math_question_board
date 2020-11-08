c8e1be8cd0f1dcdccbde5a421b01d32d3d54ab75
Eric Luby <60050306+ericluby@users.noreply.github.com>
2020-11-08 18:19:07 -0500

    removed unnecessary controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :questions, only: [:index, :create]
-      resources :messages, only: [:create]
       resources :users, only: [:show]
       get "users/current" => "users#current_user"
       resources :classrooms, only: [:index, :show, :create]
